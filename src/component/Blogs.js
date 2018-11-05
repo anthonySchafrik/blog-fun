48604fc116ecaf3b769014d2af563b6c44f97965
anthonySchafrik <anthonyschafrik@gmail.com>
2018-11-05 17:27:57 -0500

    Will render mock blogs to the screen

--- src/component/Blogs.js
+++ src/component/Blogs.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
 
+const blogs = [
+  {
+    title: "Sample Title",
+    author: "Author Name",
+    date: "time stamp",
+    body: "This is the body of the blog were all the stuff you type in goes"
+  },
+  {
+    title: "Test Title One",
+    author: "Test Author Name",
+    date: "Test time stamp",
+    body: "Test This is the body of the blog were all the stuff you type in goes"
+  }
+]
+
+
 export default class Blogs extends Component {
   render() {
     return ( 
       <div>
         <h1>Blogs</h1>
-
+        {blogs.map((blog) => {
+          return (
+            <div key={blog.title}>
+              <h3>{blog.title}</h3>
+              <div>
+                <p>{blog.author}</p>
+                <p>{blog.date}</p>
+              </div>
+              <p>{blog.body}</p>
+            </div>
+          )
+        })}
       </div>
     );
   }
