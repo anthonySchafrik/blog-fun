9a29bfd607b196c7963bb2c0846258c72ed58a57
anthonySchafrik <anthonySchafrik@gmail.com>
2018-11-12 23:08:31 -0500

    Fix the broken code i pushed up

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,11 +6,12 @@ export default class Header extends Component {
   render() {
     return (
       <div id="header-bar">
-        <p className="header-items">BlogFun</p>
-        {/* <p className="header-items">Show All Blogs</p> */}
-        <Link to="/">Show All Blogs</Link>
-        <Link to="/add/blog">Add Blog</Link>
-        {/* <p className="header-items">Post Blog</p> */}
+        <Link to="/">
+          <button>All Blogs</button>
+        </Link>
+        <Link to="/add/blog">
+          <button>Add Blog</button>
+        </Link>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter } from "react-router-dom";
 
 import reducer from "./reducers";
 import App from "./component/App.js";
@@ -10,7 +11,9 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducer)}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
